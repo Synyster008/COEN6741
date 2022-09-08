0ed536f807fef5964f31985650d5076a7438e661
sarhan8 <35826989+sarhan8@users.noreply.github.com>
2022-09-08 16:07:46 -0400

    Update alu.v

--- alu.v
+++ alu.v
@@ -6,8 +6,8 @@ output reg [31:0] out;
 
 always @( * ) begin
     case (EXECUTE_command)
-    4'b0001 : out <= in1 + in2; //add
-    4'b0010 : out <= in1 - in2; //subtract
+    4'b0001 : out <= in1 + in2; //addition 
+    4'b0010 : out <= in1 - in2; //subtraction
     4'b0011 : out <= in1 / in2; //division
     4'b0100 : out <= in1 < in2; //set less than immediate
     4'b0101 : out <= in2 << 16; //load upper immediate
