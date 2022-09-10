e3c270af7054221d4f0d629eb259e465452845ce
sarhan8 <35826989+sarhan8@users.noreply.github.com>
2022-09-09 21:20:29 -0400

    Update adder.v

--- adder.v
+++ adder.v
@@ -1,7 +1,9 @@
+// Adder Module
+
 module adder(in1, in2, out);
 
-input [31:0] in1, in2;
-output [31:0] out;
+  input [31:0] in1, in2; //Defining Input Register size
+  output [31:0] out; // Defining Input Register size
 
 assign out = in1 +in2;
 
